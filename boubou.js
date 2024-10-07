d18ed2e7cdc43841e793ce20603dfcc0437ebfa8
adil <dildil.ben@gmail.com>
2024-10-07 11:29:05 +0200

    ajout de js2

--- boubou.js
+++ boubou.js
@@ -1,6 +1,6 @@
 let psg = document.getElementById('psg')
-psg.addEventListener('mouseenter',function(){
-    psg.style.backgroundColor = 'black';
+psg.addEventListener('mouseleave',function(){
+    psg.style.backgroundColor = 'black, transparent';
 });
 
 let bille = document.getElementById('billetterie')
@@ -9,22 +9,22 @@ bille.addEventListener('mouseleave', function(){
 })
 
 let bout = document.getElementById('boutique')
-bout.addEventListener('mouseenter', function(){
+bout.addEventListener('mouseleave', function(){
     bout.style.background = 'black'
 })
 let paris = document.getElementById('myparis')
-paris.addEventListener('mouseenter', function(){
+paris.addEventListener('mouseleave', function(){
     paris.style.background = 'black'
 })
 let stadi = document.getElementById('stadiumtour')
-stadi.addEventListener('mouseenter', function(){
+stadi.addEventListener('mouseleave', function(){
     stadi.style.background = 'black'
 })
 let entrep = document.getElementById('entreprises')
-entrep.addEventListener('mouseenter', function(){
+entrep.addEventListener('mouseleave', function(){
     entrep.style.background = 'black'
 })
 let present = document.getElementById('presentation')
-present.addEventListener('mouseenter', function(){
-    present.style.background = 'blue'
+present.addEventListener('mouseleave', function(){
+    present.style.background = 'black'
 })
