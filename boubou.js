97658b449ac49a71d3446952314052c4b30da736
adil <dildil.ben@gmail.com>
2024-10-07 16:07:01 +0200

    ajout js4

--- boubou.js
+++ boubou.js
@@ -1,30 +1,54 @@
 let psg = document.getElementById('psg')
-psg.addEventListener('mouseleave',function(){
-    psg.style.backgroundColor = 'black, transparent';
+psg.addEventListener('mouseover',function(){
+    psg.style.backgroundColor = 'black,';
+});
+psg.addEventListener('mouseout',function(){
+    psg.style.backgroundColor = 'transparent,';
 });
 
 let bille = document.getElementById('billetterie')
-bille.addEventListener('mouseleave', function(){
+bille.addEventListener('mouseover', function(){
     bille.style.background = 'black'
+})
+bille.addEventListener('mouseout', function(){
+    bille.style.background = 'transparent'
 })
 
 let bout = document.getElementById('boutique')
-bout.addEventListener('mouseleave', function(){
+bout.addEventListener('mouseover', function(){
     bout.style.background = 'black'
 })
+bout.addEventListener('mouseout', function(){
+    bout.style.background = 'transparent'
+})
+
 let paris = document.getElementById('myparis')
-paris.addEventListener('mouseleave', function(){
+paris.addEventListener('mouseover', function(){
     paris.style.background = 'black'
 })
+paris.addEventListener('mouseout', function(){
+    paris.style.background = 'transparent'
+
 let stadi = document.getElementById('stadiumtour')
-stadi.addEventListener('mouseleave', function(){
+stadi.addEventListener('mouseover', function(){
     stadi.style.background = 'black'
 })
+stadi.addEventListener('mouseout', function(){
+    stadi.style.background = 'transparent'
+})
+
 let entrep = document.getElementById('entreprises')
-entrep.addEventListener('mouseleave', function(){
+entrep.addEventListener('mouseover', function(){
     entrep.style.background = 'black'
 })
+entrep.addEventListener('mouseout', function(){
+    entrep.style.background = 'transparent'
+})
+
 let present = document.getElementById('presentation')
-present.addEventListener('mouseleave', function(){
-    present.style.background = 'black'
+present.addEventListener('mouseout', function(){
+    present.style.background = 'transparent'
+})
+present.addEventListener('mouseover', function(){
+    present.style.background = 'transparent'
 })
