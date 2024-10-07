197282d165332a5f5f35713c0fe027d3d6d2cf3d
nabil <barka.nabilpro@gmail.com>
2024-10-07 09:37:01 +0100

    ajout de js

---
+++ boubou.js
@@ -0,0 +1,30 @@
+let psg = document.getElementById('psg')
+psg.addEventListener('mouseenter',function(){
+    psg.style.backgroundColor = 'black';
+});
+
+let bille = document.getElementById('billetterie')
+bille.addEventListener('mouseleave', function(){
+    bille.style.background = 'black'
+})
+
+let bout = document.getElementById('boutique')
+bout.addEventListener('mouseenter', function(){
+    bout.style.background = 'black'
+})
+let paris = document.getElementById('myparis')
+paris.addEventListener('mouseenter', function(){
+    paris.style.background = 'black'
+})
+let stadi = document.getElementById('stadiumtour')
+stadi.addEventListener('mouseenter', function(){
+    stadi.style.background = 'black'
+})
+let entrep = document.getElementById('entreprises')
+entrep.addEventListener('mouseenter', function(){
+    entrep.style.background = 'black'
+})
+let present = document.getElementById('presentation')
+present.addEventListener('mouseenter', function(){
+    present.style.background = 'blue'
+})
